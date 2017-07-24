317898bce48ad52a51df1094800137d8fdcf2bb8
Darren Cauthon <darren@cauthon.com>
2017-07-23 20:42:11 -0500

    Bring over the casting helpers from CastAs.

---
+++ src/SparkPost.Tests/CastingHelpers.cs
@@ -0,0 +1,15 @@
+﻿namespace SparkPost.Tests
+{
+    public static class CastingHelpers
+    {
+        public static T CastAs<T>(this object @object) where T : class
+        {
+            return @object as T;
+        }
+
+        public static T CastTo<T>(this object @object)
+        {
+            return (T)@object;
+        }
+    }
+}
