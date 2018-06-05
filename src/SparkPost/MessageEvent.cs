8f9e788ff9d5a3c923964d250db64a504aec33d5
Darren Cauthon <darren@cauthon.com>
2018-06-04 22:52:19 -0500

    Drop the invalid xml comment.

--- src/SparkPost/MessageEvent.cs
+++ src/SparkPost/MessageEvent.cs
@@ -326,10 +326,6 @@ namespace SparkPost
         /// </summary>
         public string RemoteAddress { get; set; }
 
-        /// <summary>
-        /// Not documented.
-        /// "remote_addr": "http://www.domain.com",
-        /// </summary>
         public string TargetLinkUrl { get; set; }
 
         public override string ToString()
